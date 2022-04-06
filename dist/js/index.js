9bcc475d33acdae922f54de23e46e162eb8645d1
TiatiniTin <aah-8@ya.ru>
2022-04-06 13:31:20 +0300

    day1

---
+++ dist/js/index.js
@@ -0,0 +1,7 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+//import {sum} from './modules/test.js';
+//document.write(sum(3,6))
+/******/ })()
+;
